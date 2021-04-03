5bc5ef81a3a8f7751a58ded65ad1e73e14d806d7
unknown <omarhindawi98@gmail.com>
2021-04-03 15:21:05 +0200

    fix: Bootstrap CSS file was missing, is added now

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
 import App from './components/App/App';
 import {BrowserRouter} from "react-router-dom";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
