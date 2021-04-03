6c6fbe1e6f1908ac85a8f771daa9889af20050e0
unknown <omarhindawi98@gmail.com>
2021-04-03 15:19:45 +0200

    feat: Home route navigation to City and Country finished

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,23 @@
+import {Link} from "react-router-dom";
+import Button from "react-bootstrap/Button";
+
+/**
+ * Home page route, acts as a portal to the two categories, City and Country
+ */
 function Home() {
     return (
-        <div></div>
+        <div className="Home">
+            <Link to="/city">
+                <Button variant="primary" size="lg" className="mr-3">
+                    City
+                </Button>
+            </Link>
+            <Link to="/country">
+                <Button variant="primary" size="lg">
+                    Country
+                </Button>
+            </Link>
+        </div>
     );
 }
 
