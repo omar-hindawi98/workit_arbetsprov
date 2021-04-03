1aa81d6ee3fcaa675838e07059e556f4ef61252c
unknown <omarhindawi98@gmail.com>
2021-04-03 15:42:33 +0200

    feat: Search box added to routes City and Country

--- src/routes/City.js
+++ src/routes/City.js
@@ -1,6 +1,15 @@
+import Search from './../components/Search/Search';
+import {useHistory} from "react-router-dom";
+
 function City() {
+    let history = useHistory();
+
+    const pushToResult = (search) => {
+        history.push("/city/" + search);
+    };
+
     return (
-        <div></div>
+        <Search type="City" placeholder="Enter a city" title="City" onSearch={pushToResult} />
     );
 }
 
--- src/routes/Country.js
+++ src/routes/Country.js
@@ -1,6 +1,16 @@
-function Country() {
+import Search from './../components/Search/Search';
+import { useHistory } from "react-router-dom";
+
+function Country(){
+    let history = useHistory();
+
+    const pushToResult = (search) => {
+        console.log(search);
+        history.push("/country/" + search);
+    };
+
     return (
-        <div></div>
+        <Search type="Country" placeholder="Enter a Country" title="Country" onSearch={pushToResult} />
     );
 }
 
