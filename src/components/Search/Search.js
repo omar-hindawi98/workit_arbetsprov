167ab3755122924478861925ff71ddbeb97c14e5
unknown <omarhindawi98@gmail.com>
2021-04-03 15:39:39 +0200

    feat: Search box now functional, needs styling

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,13 +1,50 @@
 import './Search.scss';
+import {useState} from "react";
+import { Search as SearchIcon} from 'react-bootstrap-icons';
+import {Form, Button} from 'react-bootstrap';
 
 /**
  * A search box component that will be used for parsing the search paramaters
+ * Has two props: title and placeholder
+ * One return prop: onSearch
  */
-function Search(){
+function Search(props){
+    const [search, setSearch] = useState(null);
+
+    /**
+     * Returns the search value to onSearch prop
+     * @param e, Event value
+     */
+    let performSearch = (e) => {
+        // Disable HTML Form functions
+        e.preventDefault();
+
+        // Prevent empty searches
+        if(!search || !search.trim())
+            return;
+
+        // Return search string to onSearch prop
+        props.onSearch(search);
+    };
+
+    /**
+     * Sets the state to text field value
+     * @param e, Event value
+     */
+    let handleSearchText = (e) => {
+        setSearch(e.target.value);
+    };
 
     return(
         <div>
+            <h3>Search by {props.title}</h3>
+            <Form onSubmit={performSearch}>
+                <Form.Control type="text" placeholder={props.placeholder} onChange={handleSearchText}/>
 
+                <Button variant="primary" type="submit" className="search_button">
+                    <SearchIcon/>
+                </Button>
+            </Form>
         </div>
     )
 }
