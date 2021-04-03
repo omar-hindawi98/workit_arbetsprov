4c9b09e72de1d5c82692702a4287f637ca4fb862
unknown <omarhindawi98@gmail.com>
2021-04-03 15:31:22 +0200

    init: Search component has been initialized

---
+++ src/components/Search/Search.js
@@ -0,0 +1,15 @@
+import './Search.scss';
+
+/**
+ * A search box component that will be used for parsing the search paramaters
+ */
+function Search(){
+
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default Search;
