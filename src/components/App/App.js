7e04208d13f224746de89728b4627a636593afd8
unknown <omarhindawi98@gmail.com>
2021-04-03 18:14:36 +0200

    fix: Changed title of page, and now uses config file for title

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,16 +4,17 @@ import {City, Country, Home, Result} from './../../routes/'
 import {Switch, Route, useLocation} from 'react-router-dom';
 import {CSSTransition, SwitchTransition} from "react-transition-group";
 
+import webconfig from './../../config/website';
+
 /**
  * Application Component is the main component that handles all routes
  */
 function App() {
-
     let location = useLocation();
 
     return (
     <div className="App">
-      <h1>CityPop</h1>
+      <h1>{webconfig.title}</h1>
         <SwitchTransition mode="out-in">
             <CSSTransition
                 timeout={300}
